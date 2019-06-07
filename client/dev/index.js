7a601dd578d46e0573d51487b9f4ec5cee329e7f
Ethan Watson <jellybiens@users.noreply.github.com>
2019-06-07 14:47:11 +0100

    Update index.js

--- client/dev/index.js
+++ client/dev/index.js
@@ -11,8 +11,6 @@ import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-//import ApolloClient from 'apollo-boost';
-
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -49,22 +47,6 @@ const client = new ApolloClient({
 
 
 
-
-
-// const client = new ApolloClient({
-//   uri: 'http://localhost:5000/graphql',
-//   request: operation => {
-//     operation.setContext(context => ({
-//       headers: {
-//         ...context.headers,
-//         authorization:  localStorage.getItem("token") ? `Bearer ${JSON.parse(localStorage.getItem("token")).token}` : "",
-//       },
-//     }));
-//   }
-//
-// });
-
-
 ReactDOM.render(
     <BrowserRouter>
       <ApolloProvider client={client}>
@@ -73,45 +55,3 @@ ReactDOM.render(
     </BrowserRouter>
 
     , document.getElementById('root'));
-
-
-
-
-
-
-    //
-    // const defaultOptions = {
-    //   watchQuery: {
-    //     fetchPolicy: 'network-only',
-    //     errorPolicy: 'ignore',
-    //   },
-    //   query: {
-    //     fetchPolicy: 'network-only',
-    //     errorPolicy: 'all',
-    //   },
-    // };
-    //
-    // const httpLink = createHttpLink({
-    //   uri: 'http://localhost:5000/graphql',
-    // });
-    //
-    // const authLink = setContext((_, { headers }) => {
-    //   const token = JSON.parse(localStorage.getItem("token")).token;
-    //   return {
-    //     headers: {
-    //       ...headers,
-    //       authorization: token ? `Bearer ${token}` : "",
-    //     }
-    //   }
-    // });
-    //
-    // const client = new ApolloClient({
-    //   link: authLink.concat(httpLink),
-    //   //defaultOptions: defaultOptions,
-    //   cache: new InMemoryCache()
-    // });
-    //
-    //
-    //
-    //
-    //
